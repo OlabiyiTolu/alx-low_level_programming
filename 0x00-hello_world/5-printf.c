ec0bd6e26bb821f159174e81ce0ea567ea739e53
OlabiyiTolu <OlabiyiTolu@gmail.com>
2023-07-13 11:44:11 -0700

    UPDATE printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
  * main - function tthat uses print f
+ * Description: print f
+ * return - Always 0(success
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
