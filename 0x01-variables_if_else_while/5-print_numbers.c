87883bbee7e2a28b10219e0f3316c39e8670f9a3
OlabiyiTolu <OlabiyiTolu@gmail.com>
2023-07-14 15:14:45 -0700

    5-print_number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print binary
+ * Description: print all binarystarting from zero
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b=0; b <= 10; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
