66d7808428d4194da2780b3cc18260336ca9fd06
OlabiyiTolu <OlabiyiTolu@gmail.com>
2023-07-14 12:40:52 -0700

    UPDATE 0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ int main(void)
 		printf("The number is positive");
 	} else if(n==0){
 		printf("The number is zero");
-	} else(n<0){
+	} else {
 		printf("The number is negative");
 	}
 	return (0);
