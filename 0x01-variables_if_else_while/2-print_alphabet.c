4b01803e49c48b07a8d2d96f905604f9a8b4af8a
OlabiyiTolu <OlabiyiTolu@gmail.com>
2023-07-14 14:10:25 -0700

    2-pint_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ * 
+ * Description: print alphabet in lower case
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char a;
+	for ( c = 'a', c <= 'z', c++ )
+	{
+		putchar(c);
+	}
+	putchar("\n");
+	return (0);
+}
