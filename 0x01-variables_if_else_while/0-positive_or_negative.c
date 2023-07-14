ac26518d43f80f1dabc302907e3afde1d2a24424
OlabiyiTolu <OlabiyiTolu@gmail.com>
2023-07-14 12:36:35 -0700

    UPDATE 0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,9 +9,9 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0){
+	if(n>0){
 		printf("The number is positive");
-	} else if(n=0){
+	} else if(n==0){
 		printf("The number is zero");
 	} else (n<0){
 		printf("The number is negative");
