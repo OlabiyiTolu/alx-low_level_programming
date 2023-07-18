734e33773b584fbb11715d25796f730487524c1a
OlabiyiTolu <OlabiyiTolu@gmail.com>
2023-07-18 09:51:14 -0700

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ *
+ * Return: return0
+ */
+
+void print_alphabet(void)	
+{	
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
