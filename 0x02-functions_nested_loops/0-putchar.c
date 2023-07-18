ef19a33b1d17854a2909d3e42aff2ddb6226a760
OlabiyiTolu <OlabiyiTolu@gmail.com>
2023-07-18 09:26:10 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print putchar
+ * Description: function that prints putchar
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	return 0;
+}
