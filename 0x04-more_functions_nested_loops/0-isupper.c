16038ca1d86b3ac2231ad4a900109a4670ca0777
Tolu <olabiyitolu@gmail.com>
2023-07-21 15:30:54 +0000

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isupper - check forupper case
+* Return: return 1 if upper and 0 if otherwise 
+*/
+
+int _isupper(int c)
+{
+	if((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+}
