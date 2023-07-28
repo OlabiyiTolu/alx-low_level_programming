b7cba5e56f1db252bbd088169ea0cb6646c4711f
OlabiyiTolu <OlabiyiTolu@gmail.com>
2023-07-28 13:23:56 -0700

    UPDATE

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * leet - function that encodes a string into 1337.
  * @str: string that will be encoded
@@ -7,22 +8,21 @@
 
 char *leet(char *str)
 {
-    int index1 = 0, index2;
-    char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
-    char replacements[8] = {'0', '1', '?', '3', '4', '?', '?', '7'};
-
-    while (str[index1])
-    {
-        for (index2 = 0; index2 <= 7; index2++)
-        {
-            if (str[index1] == leet[index2] || str[index1] - 32 == leet[index2])
-            {
-                str[index1] = replacements[index2];
-                break;
-            }
-        }
-        index1++;
-    }
-    return (str);
+	int index1 = 0, index2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	char replacements[8] = {'0', '1', '?', '3', '4', '?', '?', '7'};
+	while (str[index1])
+	{
+		for (index2 = 0; index2 <= 7; index2++)
+		{
+			if (str[index1] == leet[index2] || str[index1] - 32 == leet[index2])
+			{
+				str[index1] = replacements[index2];
+				break;
+			}
+		}
+		index1++;
+	}
+	return (str);
 }
 
