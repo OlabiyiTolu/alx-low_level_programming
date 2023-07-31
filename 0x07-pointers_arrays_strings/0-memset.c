94b38b5bb1464e5d52d02930d27629aa7e855d97
OlabiyiTolu <OlabiyiTolu@gmail.com>
2023-07-31 11:43:15 -0700

    UPDATE memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,13 @@
  * Return: return s
  */
 
-char *_memset(char *s, char b, unsigned int n) {
-  char *p = s;
-  while (n--) {
-    *p++ = b;
-  }
-  return s;
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *p = s;
+	
+	while (n--) 
+	{
+		*p++ = b;
+	}
+	return (s);
 }
-
