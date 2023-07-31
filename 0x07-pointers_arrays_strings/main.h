412126ae7fadeb9fd2c1d62858822a7f2369ef93
OlabiyiTolu <OlabiyiTolu@gmail.com>
2023-07-31 11:39:57 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void simple_print_buffer(char *buffer, unsigned int size);
+int main(void);
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+
+#endif
