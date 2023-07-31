5fbc0f7296a31c9dff4934ee0946dc366b744ccd
OlabiyiTolu <OlabiyiTolu@gmail.com>
2023-07-31 12:52:23 -0700

    strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ int main(void);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
+char *_strchr(char *s, char c);
 int _putchar(char c);
 
 #endif
