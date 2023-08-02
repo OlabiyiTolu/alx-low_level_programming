753efb8aeaf217fa4264ec1d550edfac46531718
OlabiyiTolu <OlabiyiTolu@gmail.com>
2023-08-02 15:30:04 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * factorial - function to print factorial
+ * @n: input variable
+ *
+ * Return: Always return 0
+ */
+
+int factorial(int n)
+{
+	if (n == 1)
+	{
+		return(1);
+	}
+	else if (n < 1)
+	{
+		return(-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
