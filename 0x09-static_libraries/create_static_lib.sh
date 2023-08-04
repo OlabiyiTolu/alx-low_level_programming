d3318ade5709e7c9d6dc9036edd2678f04f30945
OlabiyiTolu <OlabiyiTolu@gmail.com>
2023-08-04 05:40:21 -0700

    static library script

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -8,6 +8,3 @@ c_files=$(find $current_dir -type f -name "*.c")
 
 # Create the static library
 ar -rcs liball.a $c_files
-
-# Display a message
-echo "Static library liball.a created successfully!"
