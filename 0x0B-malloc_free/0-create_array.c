47a6105a169216316466472a541948f30dbcc2fe
OlabiyiTolu <OlabiyiTolu@gmail.com>
2023-08-08 03:34:27 -0700

    update malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,18 +13,17 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int index;
 
-	if(size == 0)
+	if (size == 0)
 	{
 		return (NULL);
 	}
 
 	array = malloc(sizeof(char) * size);
-	
 	if (array == 0)
 	{
 		return (NULL);
 	}
-	for(index = 0; index < size; index++)
+	for (index = 0; index < size; index++)
 	{
 		array[index] = c;
 	}
