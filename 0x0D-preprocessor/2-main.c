879015b00fc8106aa9422d25f3fcb470d9ffc03d
OlabiyiTolu <OlabiyiTolu@gmail.com>
2023-08-14 14:59:21 -0700

    UPDATE

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
- * main - function that prints the name of the file itwas compiled from
+ * main - prints the name of the file itwas compiled from
  *
  * Return: return name of file
  */
+
 int main(void)
 {
 	char *filename = __FILE__;
+
 	printf("%s\n", filename);
 	return (0);
 }
